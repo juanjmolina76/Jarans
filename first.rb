ce6059b671d1ffa1ba1c7c35e734e21d83f8a0b3
juanjmolina76 <molinajuanj@hotmail.com>
2023-08-28 20:21:08 -0300

    comentando cambios hacia github

---
+++ first.rb
@@ -0,0 +1,10 @@
+text = "El dicho al hecho hay un largo trecho, si no hay nada que decir. Tal vez lo que se diga no fue dicho.
+Antes de un cuento"
+text.downcase!
+freqs = {}
+freqs.default =0
+text.each_char {|x|freqs[x] +=1}
+("a".."z").each { |y| puts "#{y} : #{freqs[y]}"}
+=begin
+probando los comentarios y subiendo en github
+=end
